0c4130881770a2409d11b2b9125c1e85740a4b2f
Branka Martinovic <branka.martinovic@live.com>
2017-10-07 17:26:36 +0200

    Showing stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
-exports.homePage = (req, res) => {
-  res.render('index', { title: 'Home' })
-};
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'ADD STORE' })
 };
@@ -13,8 +9,12 @@ exports.createStore = async (req, res) => {
   req.body.tags = req.body.tags.split(',').map(function(tag) {
     return tag.trim();
   });
-  const store = new Store(req.body);
-  await store.save();
+  const store = await(new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect('/')
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'STORES', stores })
 };
