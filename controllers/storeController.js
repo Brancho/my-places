1ed7c3610f8d6672f55bb13c21473a5e29f4417f
Branka Martinovic <branka.martinovic@live.com>
2017-10-08 15:26:47 +0200

    Editing stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,9 +6,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  req.body.tags = req.body.tags.split(',').map(function(tag) {
-    return tag.trim();
-  });
+  req.body.tags = trimTags(req.body.tags);
   const store = await(new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
@@ -18,3 +16,34 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'STORES', stores })
 };
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `EDIT ${store.name.toUpperCase()}`, store })
+};
+
+
+exports.updateStore = async (req, res) => {
+  req.body.tags = trimTags(req.body.tags);
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name}.`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+
+function trimTags(tags){
+  return tags.split(',').map(function(tag) {
+    return tag.trim();
+  });
+}
+
+
+
+
+
+
+
+
